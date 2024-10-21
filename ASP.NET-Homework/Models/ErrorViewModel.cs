fd3f4eb6f58cdd489f6b9730f3825ba7a9ac942e
Nikita Developer <nik0609tak@gmail.com>
2024-10-21 20:58:07 +0200

    Initial commit

---
+++ ASP.NET-Homework/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ASP.NET_Homework.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
