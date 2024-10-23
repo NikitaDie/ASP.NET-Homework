a34f0be0be3212c1f3c6f6f408e15d4160bc73f0
Nikita Developer <nik0609tak@gmail.com>
2024-10-23 19:34:26 +0200

    Food

---
+++ Homework_Food_02/Controllers/FoodController.cs
@@ -0,0 +1,36 @@
+﻿using Homework_Food_02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_Food_02.Controllers;
+
+public class FoodController : Controller
+{
+    private static List<Food> _foodList = new List<Food>
+    {
+        new Food { Id = 1, Name = "Борщ", Weight = 350, Price = 50 },
+        new Food { Id = 2, Name = "Вареники", Weight = 200, Price = 40 }
+    };
+    
+    public IActionResult Index()
+    {
+        return View(_foodList);
+    }
+    
+    [HttpGet]
+    public IActionResult AddFood()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult AddFood(Food food)
+    {
+        if (ModelState.IsValid)
+        {
+            food.Id = _foodList.Max(f => f.Id) + 1;
+            _foodList.Add(food);
+            return RedirectToAction("Index");
+        }
+        return View(food);
+    }
+}
---
+++ Homework_Food_02/Models/Food.cs
@@ -0,0 +1,9 @@
+﻿namespace Homework_Food_02.Models;
+
+public class Food
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Weight { get; set; }
+    public decimal Price { get; set; }
+}
